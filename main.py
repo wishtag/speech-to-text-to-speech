88145936407ab2187c8cc23e456d9a4d53ed9ad4
HaydenPhillips777 <hlphillips1@ualr.edu>
2025-04-02 15:47:37 -0500

    Set TTS to ignore commas

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ import pyttsx3
 import threading
 
 engine = pyttsx3.init()
+engine.setProperty('rate', 250)
 
 def speak(text):
     try:
         engine.endLoop()
     except:
         pass
+    text = text.replace(",","")
     engine.say(text)
     engine.runAndWait()
 
 if __name__ == '__main__':
     print("Wait until it says 'speak now'")
+
     recorder = AudioToTextRecorder()
 
     while True:
-        print(recorder.text())
-        t = threading.Thread(target=speak, args=(recorder.text(),))
+        text = recorder.text()
+        print(text)
+        t = threading.Thread(target=speak, args=(text,))
         t.start()
